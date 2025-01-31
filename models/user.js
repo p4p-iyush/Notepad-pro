200cca314881f4be796254839c25c0ebf8afe883
Piyush Jain <jain4piyush@gmail.com>
2025-01-31 19:10:33 +0530

    changes in user.js by piyush

--- models/user.js
+++ models/user.js
@@ -1,5 +1,18 @@
-const mongoose =require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/TextEditor');
+
+const mongoose = require('mongoose');
+
+// Use environment variable for the MongoDB URI
+const mongoURI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/TextEditor'; // Default for local testing
+
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log("Connected to MongoDB!");
+}).catch((err) => {
+  console.log("Error connecting to MongoDB:", err);
+});
+
 
 const UserSchema= mongoose.Schema({
   title:{

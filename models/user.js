9818ea8146ffd4d883d45b888c51a4a2ffd0c2f0
Piyush Jain <jain4piyush@gmail.com>
2025-01-31 22:23:45 +0530

    changes in user.js

--- models/user.js
+++ models/user.js
@@ -5,10 +5,7 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 // Connect to MongoDB Atlas using the connection string from .env
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('MongoDB connected successfully'))
 .catch((err) => console.log('MongoDB connection error:', err));
 
